43863d3491aa3feeb860b4fdec0b7aa6d4482b10
Konstantin Prokhorov <fandom170@gmail.com>
2020-04-12 19:03:31 +0300

    Next commit. Opening of login page and login page checks has been added

--- test_main_page.py
+++ test_main_page.py
@@ -1,22 +1,18 @@
-#command for running
+from .pages.login_page import LoginPage
+from .pages.main_page import MainPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    page.should_be_login_link()
 
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+    login_page.should_be_login_url()
+    login_page.should_be_register_form()
+    login_page.should_be_login_form()
 
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
-
-'''
-def test_add_to_cart(browser):
-    page = ProductPage(url="", browser)   # инициализируем объект Page Object
-    page.open()                           # открываем страницу в браузере
-    page.should_be_add_to_cart_button()   # проверяем что есть кнопка добавления в корзину
-    page.add_product_to_cart()            # жмем кнопку добавить в корзину
-    page.should_be_success_message()      # проверяем что есть сообщение с нужным текстом
-'''
